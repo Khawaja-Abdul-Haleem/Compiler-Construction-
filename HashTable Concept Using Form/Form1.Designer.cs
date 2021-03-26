59f09f991012dd675df90fc718e1c2d520f526af
Khawaja Abdul Haleem <59179832+Khawaja-Abdul-Haleem@users.noreply.github.com>
2021-03-26 13:52:37 +0500

    Add files via upload

---
+++ HashTable Concept Using Form/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace HASH_Table
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_ID = new System.Windows.Forms.TextBox();
+            this.tb_name = new System.Windows.Forms.TextBox();
+            this.tb_des = new System.Windows.Forms.TextBox();
+            this.tb_sal = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.cb_show = new System.Windows.Forms.ComboBox();
+            this.btn_show = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tb_ID
+            // 
+            this.tb_ID.Location = new System.Drawing.Point(221, 37);
+            this.tb_ID.Name = "tb_ID";
+            this.tb_ID.Size = new System.Drawing.Size(166, 20);
+            this.tb_ID.TabIndex = 0;
+            // 
+            // tb_name
+            // 
+            this.tb_name.Location = new System.Drawing.Point(221, 63);
+            this.tb_name.Name = "tb_name";
+            this.tb_name.Size = new System.Drawing.Size(166, 20);
+            this.tb_name.TabIndex = 1;
+            // 
+            // tb_des
+            // 
+            this.tb_des.Location = new System.Drawing.Point(221, 89);
+            this.tb_des.Name = "tb_des";
+            this.tb_des.Size = new System.Drawing.Size(166, 20);
+            this.tb_des.TabIndex = 2;
+            // 
+            // tb_sal
+            // 
+            this.tb_sal.Location = new System.Drawing.Point(221, 115);
+            this.tb_sal.Name = "tb_sal";
+            this.tb_sal.Size = new System.Drawing.Size(166, 20);
+            this.tb_sal.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(131, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(131, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(131, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Designation";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(131, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Salary";
+            // 
+            // btn_add
+            // 
+            this.btn_add.Location = new System.Drawing.Point(179, 171);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(75, 23);
+            this.btn_add.TabIndex = 8;
+            this.btn_add.Text = "ADD";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // cb_show
+            // 
+            this.cb_show.FormattingEnabled = true;
+            this.cb_show.Items.AddRange(new object[] {
+            "ID",
+            "Name",
+            "Designation",
+            "Salary"});
+            this.cb_show.Location = new System.Drawing.Point(285, 171);
+            this.cb_show.Name = "cb_show";
+            this.cb_show.Size = new System.Drawing.Size(84, 21);
+            this.cb_show.TabIndex = 9;
+            // 
+            // btn_show
+            // 
+            this.btn_show.Location = new System.Drawing.Point(179, 215);
+            this.btn_show.Name = "btn_show";
+            this.btn_show.Size = new System.Drawing.Size(75, 23);
+            this.btn_show.TabIndex = 10;
+            this.btn_show.Text = "Show";
+            this.btn_show.UseVisualStyleBackColor = true;
+            this.btn_show.Click += new System.EventHandler(this.btn_show_Click);
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Location = new System.Drawing.Point(285, 215);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(75, 23);
+            this.btn_exit.TabIndex = 11;
+            this.btn_exit.Text = "Exit";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btn_show);
+            this.Controls.Add(this.cb_show);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_sal);
+            this.Controls.Add(this.tb_des);
+            this.Controls.Add(this.tb_name);
+            this.Controls.Add(this.tb_ID);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_ID;
+        private System.Windows.Forms.TextBox tb_name;
+        private System.Windows.Forms.TextBox tb_des;
+        private System.Windows.Forms.TextBox tb_sal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.ComboBox cb_show;
+        private System.Windows.Forms.Button btn_show;
+        private System.Windows.Forms.Button btn_exit;
+    }
+}
+
---
+++ HashTable Concept Using Form/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace HASH_Table
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            Hashtable ht = new Hashtable()
+            {
+                {"Id", int.Parse(tb_ID.Text)},
+                {"Name", tb_name.Text},
+                {"Designation", tb_des.Text},
+                {"Salary", int.Parse(tb_sal.Text)}
+
+            };
+            ICollection keys = ht.Keys;
+
+            
+            foreach (string k in keys)
+            {
+                MessageBox.Show(k + ":" + ht[k]);
+            }
+            MessageBox.Show("Inserted Sucessfully");
+
+        }
+
+        private void btn_show_Click(object sender, EventArgs e)
+        {
+            if (cb_show.SelectedIndex == 0) {
+                MessageBox.Show(tb_ID.Text);
+            }
+            else if (cb_show.SelectedIndex == 1)
+            {
+                MessageBox.Show(tb_name.Text);
+            }
+            else if (cb_show.SelectedIndex == 2)
+            {
+                MessageBox.Show(tb_des.Text);
+            }
+            else if (cb_show.SelectedIndex == 3)
+            {
+                MessageBox.Show(tb_sal.Text);
+            }
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
