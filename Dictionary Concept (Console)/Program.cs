d50ff9df8d4c4e29921934ed4a90da3f60c86868
Khawaja Abdul Haleem <59179832+Khawaja-Abdul-Haleem@users.noreply.github.com>
2021-03-26 13:57:18 +0500

    Add files via upload

---
+++ Dictionary Concept (Console)/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Dictionary <string, string> dict = new Dictionary<string,string>();
+            dict.Add("Amazing", "Incredible");
+            dict.Add("Anger", "Enrage");
+            dict.Add("Angry", "Furious");
+            dict.Add("Answer", "Reply");
+            dict.Add("Ask", "Query");
+            dict.Add("Brave", "Fearless");
+            dict.Add("Big", "Huge");
+            dict.Add("Break", "Fracture");
+            dict.Add("Bright", "Vivid");
+            dict.Add("Cry", "Weep");
+
+            foreach (var item in dict) {
+                Console.WriteLine("Key is: " + item.Key + "\t\tValue is: " + item.Value);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
